168eae5d773829950e72a489f10a58aa2768206e
HiJacker <H744205975@163.com>
2018-03-09 17:51:21 +0800

    manage update

--- manage.py
+++ manage.py
@@ -2,15 +2,13 @@ from flask_script import Manager
 from my_blog import app
 from models import SuperUser,Article
 from exts import db
+from flask_migrate import Migrate,MigrateCommand
 
 
 manager = Manager(app)
+Migrate(app,db)
+manager.add_command('db',MigrateCommand)
 
-@manager.command
-def init():
-    db.drop_all()
-    db.create_all()
-    print('数据库初始化完毕')
 
 @manager.option('-u','--username',dest='username')
 @manager.option('-p','--password',dest='password')
